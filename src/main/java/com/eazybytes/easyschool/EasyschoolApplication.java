b9795cd26536fba7a3ac85e860de5f88ef3f69fc
wesley <brunow9@outlook.com>
2023-06-20 17:40:40 -0300

    primeiro commit

---
+++ src/main/java/com/eazybytes/easyschool/EasyschoolApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes.easyschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eazybytes/easyschool/EasyschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.easyschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
