65b69a983aa1c3d68b83e0b21e2c2da1831d6bc3
wesley <brunow9@outlook.com>
2023-06-20 21:14:34 -0300

    adicão da página inicial e seus arquivos

--- src/main/java/com/eazybytes/easyschool/controller/HomeController.java
+++ src/main/java/com/eazybytes/easyschool/controller/HomeController.java
@@ -1,11 +1,12 @@
 package com.eazybytes.easyschool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/home")
+    @RequestMapping(value={"", "/", "/home"})
     public String displayHomePage() {
         return  "home.html";
     }
