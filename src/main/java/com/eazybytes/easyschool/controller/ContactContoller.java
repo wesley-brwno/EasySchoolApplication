05b012ea04312d939354fa6dd54825501e3b5ca4
wesley <brunow9@outlook.com>
2023-06-21 15:52:57 -0300

    Recebimento de dados pelo método post

--- src/main/java/com/eazybytes/easyschool/controller/ContactContoller.java
+++ src/main/java/com/eazybytes/easyschool/controller/ContactContoller.java
@@ -1,13 +1,32 @@
 package com.eazybytes.easyschool.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ContactContoller {
 
+    private static Logger log = LoggerFactory.getLogger(ContactContoller.class);
     @RequestMapping("/contact")
     public String displayContactPage() {
         return "contact.html";
+    }
+
+    @PostMapping(value= "/saveMsg")
+    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum,
+                                    @RequestParam String email, @RequestParam String subject,
+                                    @RequestParam String message) {
+
+        log.info("Name : " + name);
+        log.info("Mobile Numer : " + mobileNum);
+        log.info("Email Address : " + email);
+        log.info("Subject : " + subject);
+        log.info("Message : " + message);
+        return new ModelAndView("redirect:/contact");
     }
 }
