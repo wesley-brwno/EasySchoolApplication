5600fd274639fa99e9d6c05fbf528c977fabe337
wesley <brunow9@outlook.com>
2023-06-23 16:18:41 -0300

    Configuração costumizada do spring security

--- src/main/java/com/eazybytes/easyschool/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/easyschool/config/ProjectSecurityConfig.java
@@ -14,14 +14,28 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
         // Permit all reqeusts inside the Web Application
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+//        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+//        http.formLogin(withDefaults());
+//        http.httpBasic(withDefaults());
 
         //Deny all Request inside the Web Application
 //        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
 //        http.formLogin(withDefaults());
 //        http.httpBasic(withDefaults());
+
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("", "/", "/home").permitAll()
+                .requestMatchers("/holidays/**").permitAll()
+                .requestMatchers("/contact").permitAll()
+                .requestMatchers("/saveMsg").permitAll()
+                .requestMatchers("/courses").permitAll()
+                //.requestMatchers("/courses").authenticated()
+                .requestMatchers("/about").permitAll()
+                .requestMatchers("/assets/**").permitAll()
+                .anyRequest().authenticated());
+
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
 
         return http.build();
     }
